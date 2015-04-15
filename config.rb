bfeba4cf57c763ac63044a563e799fbf80a65716
Stephen Caudill <scaudill@gmail.com>
2015-04-15 09:50:08 -0600

    Configure build output settings

--- config.rb
+++ config.rb
@@ -8,18 +8,9 @@ activate :livereload
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
-
-  # Enable cache buster
-  # activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
-
+  activate :minify_css
+  activate :minify_javascript
+  activate :asset_hash
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
 end
